6db8b2814f3567b90649189ed1d21c6ffad35e43
41118142 <148747880+41118142@users.noreply.github.com>
2025-04-15 15:27:03 +0800

    Update views.py

--- studentsapp/views.py
+++ studentsapp/views.py
@@ -16,3 +16,12 @@ def listall(request):
 def index(request):  
     students = student.objects.all().order_by('id')  # 首頁顯示全部
     return render(request, "index.html", {'students': students})
+from django.shortcuts import render
+from studentsapp.models import student
+from datetime import date
+
+def initdata(request):
+    if not student.objects.filter(cName="林宸宇").exists():
+        student.objects.create(cName="林宸宇", cSex="M", cBirthday=date(2004,6,22),
+                               cEmail="41118142@school.com", cPhone="0909777615", cAddr="鳥松區87號")
+    return render(request, "index.html", {"students": student.objects.all()})
